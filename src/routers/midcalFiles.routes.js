9f9163124998f85b2ac407ab3fcc61ab442a7346
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-14 17:03:45 +0500

    feature: create routes for medical files

---
+++ src/routers/midcalFiles.routes.js
@@ -0,0 +1,19 @@
+var express = require("express");
+const {
+  updateMedicalFile,
+  createMedicalFile,
+  getMedicalFiles,
+} = require("../controllers/medicalFilesController");
+const { authentication } = require("../utilities/auth");
+const uploader = require("../utilities/uploader");
+var router = express.Router();
+
+router.post("/:beneficiaryId", authentication, createMedicalFile);
+router.get("/", authentication, getMedicalFiles);
+router.post(
+  "/update/:id",
+  uploader.singleFileUpload.any({ name: "medicalFile" }),
+  updateMedicalFile
+);
+
+module.exports = router;
