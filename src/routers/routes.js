a320486dfe1dea026d74a10fdb4e2bb3225c3269
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-15 16:19:25 +0500

    resolve conflicts

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -15,6 +15,7 @@ const smsRouter = require("./sms.routes");
 const medicalFileRouter = require("./midcalFiles.routes");
 const smsRouter = require("./sms.routes");
 
+const medicalFileRouter = require("./midcalFiles.routes");
 const API_VERSION = "/v1";
 
 router.use(API_VERSION + "/login", loginRouter);
@@ -29,6 +30,7 @@ router.use(API_VERSION + "/schedules", schedulesRouter);
 router.use(API_VERSION + "/sms", smsRouter);
 router.use(API_VERSION + "/medicalFiles", medicalFileRouter);
 router.use(API_VERSION + "/sms", smsRouter);
+router.use(API_VERSION + "/medicalFiles", medicalFileRouter);
 // router.use("/claims", claimsRouter);
 
 module.exports = router;
