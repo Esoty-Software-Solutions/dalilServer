f9194758ae7386d5c0e0009da4b8d5fa32f62e29
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-20 17:39:45 +0500

    feature:Update structure to es6

--- src/services/medicalCenterServices.js
+++ src/services/medicalCenterServices.js
@@ -1,25 +1,35 @@
-const MedicalCenterSchema = require("../schemas/medicalCenterSchema");
+import MedicalCenterSchema from "../schemas/medicalCenterSchema.js";
 
-exports.createMedicalCenter = async (query) => {
+const createMedicalCenter = async (query) => {
   return await MedicalCenterSchema.create(query);
 };
 
-exports.updateMedicalCenter = async (query, data) => {
+const updateMedicalCenter = async (query, data) => {
   return await MedicalCenterSchema.findOneAndUpdate(query, data, {
     new: true,
   });
 };
 
-exports.deleteMedicalCenter = async (query) => {
+const deleteMedicalCenter = async (query) => {
   return await MedicalCenterSchema.findOneAndDelete(query);
 };
 
-exports.getAllMedicalCenters = async (query, limit) => {
+const getAllMedicalCenters = async (query, limit) => {
   return await MedicalCenterSchema.find(query).limit(limit).select("-__v ");
 };
 
-exports.getMedicalCenterDetails = async (query) => {
+const getMedicalCenterDetails = async (query) => {
   return await MedicalCenterSchema.findOne(query).select(
     "-__v -createdAt -updatedAt"
   );
 };
+
+const MedicalCenterServices = {
+  createMedicalCenter,
+  updateMedicalCenter,
+  getAllMedicalCenters,
+  getMedicalCenterDetails,
+  deleteMedicalCenter,
+};
+
+export default MedicalCenterServices;
