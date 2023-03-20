3cb2dc63067afcbbe49d9dd0d4032aaebc3f5b46
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-03-20 15:16:39 +0500

    feature:Update structure to es6

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,19 @@
-const dotenv = require("dotenv");
-const path = require("path");
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, ".env") });
+
 dotenv.config({
   path: path.resolve(__dirname, `../../${process.env.NODE_ENV}.env`),
 });
+
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-module.exports = {
+const config = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3000,
   mongouri: process.env.mongouri,
   jwtSecret: process.env.jwtSecret,
 };
+export default config;
