e425b9d3e7c060039ae1689a5d4eb37d576dacef
Waleed Saifi <107859234+waleedsaifi0890@users.noreply.github.com>
2023-03-28 16:25:33 +0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ dotenv.config({
   path: path.resolve(__dirname, `../../${process.env.NODE_ENV}.env`),
 });
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-console.log("process.env.MONGOURI", process.env.MONGOURI, process.env.mongouri);
+console.log("process.env.MONGOURI", process.env.jwtSecret, process.env.mongouri);
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3000,
