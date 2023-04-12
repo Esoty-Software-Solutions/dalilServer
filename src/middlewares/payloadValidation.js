9891d65d0f1c5f1f71afe78568d023c65301019a
Aarush-Virtual <115481294+Aarush-Virtual@users.noreply.github.com>
2023-04-12 16:20:42 +0530

    added try catch block to handle error in payload validation

--- src/middlewares/payloadValidation.js
+++ src/middlewares/payloadValidation.js
@@ -4,13 +4,20 @@ const {
 
 exports.customValidation = (joiValidator, payloadType) => {
     // payload type can be body or query as of now
-    return (req, res, next) => {
-        const payload = req[payloadType];
-        const {error} = joiValidator.validate(payload);
-        if (error) {
-            badRequestErrorResponse(res, error.message.replaceAll('"', ""))
-        } else {
-            next();
+    try{
+        return (req, res, next) => {
+            const payload = req[payloadType];
+            const {error} = joiValidator.validate(payload);
+            if (error) {
+                badRequestErrorResponse(res, error.message.replaceAll('"', ""))
+            } else {
+                next();
+            }
+        }
+    }catch(err) {
+        return (req, res , next) => {
+            console.log(err);
+            badRequestErrorResponse(res, "Payload Not Valid");
         }
     }
 }
