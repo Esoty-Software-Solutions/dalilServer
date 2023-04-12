0de127c460d010aac0fb4d0c7bfbab10f0d2b383
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-04-12 15:10:03 +0500

    feature:Create fuzzy search in all city controller

--- src/controllers/cityController.js
+++ src/controllers/cityController.js
@@ -6,7 +6,6 @@ const {
   serverErrorResponse,
 } = require("../utilities/response");
 const { messageUtil } = require("../utilities/message");
-
 const city = {
   // Add Appointment Status Enum
 
@@ -42,12 +41,35 @@ const city = {
 
   getAllCity: async (req, res) => {
     try {
-      let query = {
-        limit: req.query.limit,
-        skip: req.query.skip,
-      };
-      let objectArray = await cityServices.getAllCity(query);
+      // let query = {
+      //   limit: req.query.limit,
+      //   skip: req.query.skip,
+      // };
 
+      const searchFields = ["backendName", "englishName"];
+
+      // Define the search query
+      const query1 = {
+        $or: searchFields.map((field) => ({
+          [field]: {
+            $regex: req.query.searchQuery.split("").join(".*"),
+            $options: "i", // Case-insensitive search
+          },
+        })),
+      };
+      let objectArray = await cityServices.fuzzySearch(query1);
+      ///This solution works but we need to search on db level
+      // let list = await cityServices.getAllCity(query);
+      // const options = {
+      //   keys: ["backendName", "englishName"],
+      // };
+      // const fuse = new Fuse(list, options);
+      // const result = fuse.search(req.query.search);
+      // console.log(
+      //   "🚀 ~ file: cityController.js:55 ~ getAllCity: ~ result:",
+      //   result
+      // );
+      // const objectArray = result.map((r) => r.item);
       return successResponse(
         res,
         messageUtil.success,
