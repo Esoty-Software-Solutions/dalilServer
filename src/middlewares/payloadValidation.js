7400b687df986f6ca9a39ea4a73dc2fd3b7573d6
Abdul <raoph40@gmail.com>
2023-04-11 23:58:55 -0600

    disable validation

--- src/middlewares/payloadValidation.js
+++ src/middlewares/payloadValidation.js
@@ -4,13 +4,14 @@ const {
 
 exports.customValidation = (joiValidator, payloadType) => {
     // payload type can be body or query as of now
-    return (req, res, next) => {
-        const payload = req[payloadType];
-        const {error} = joiValidator.validate(payload);
-        if (error) {
-            badRequestErrorResponse(res, error.message.replaceAll('"', ""))
-        } else {
-            next();
-        }
-    }
+    next();
+    // return (req, res, next) => {
+    //     const payload = req[payloadType];
+    //     const {error} = joiValidator.validate(payload);
+    //     if (error) {
+    //         badRequestErrorResponse(res, error.message.replaceAll('"', ""))
+    //     } else {
+    //         next();
+    //     }
+    // }
 }
