458ed41c67122410f0b1f91f04900a7b05fe339f
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-04-14 12:51:19 +0500

    trying to resolve conflicts

--- src/middlewares/payloadValidation.js
+++ src/middlewares/payloadValidation.js
@@ -1,28 +1,26 @@
-const {
-    badRequestErrorResponse
-} = require("../utilities/response");
+const { badRequestErrorResponse } = require("../utilities/response");
 
-exports.customValidation = (joiValidator, payloadType , isFile = false) => {
-    // payload type can be body or query as of now
-    try{
-        return (req, res, next) => {
-            const payload = req[payloadType];
-            if(isFile) {
-                if(!req.file) {
-                    return badRequestErrorResponse(res, "Please provide file");
-                }
-            }
-            const {error} = joiValidator.validate(payload);
-            if (error) {
-                return badRequestErrorResponse(res, error.message.replaceAll('"', ""))
-            } else {
-                next();
-            }
+exports.customValidation = (joiValidator, payloadType, isFile = false) => {
+  // payload type can be body or query as of now
+  try {
+    return (req, res, next) => {
+      const payload = req[payloadType];
+      if (isFile) {
+        if (!req.file) {
+          return badRequestErrorResponse(res, "Please provide file");
         }
-    }catch(err) {
-        return (req, res , next) => {
-            console.log(err);
-            return badRequestErrorResponse(res, "Request body Not Valid");
-        }
-    }
-}
+      }
+      const { error } = joiValidator.validate(payload);
+      if (error) {
+        return badRequestErrorResponse(res, error.message.replaceAll('"', ""));
+      } else {
+        next();
+      }
+    };
+  } catch (err) {
+    return (req, res, next) => {
+      console.log(err);
+      return badRequestErrorResponse(res, "Request body Not Valid");
+    };
+  }
+};
--- src/utilities/uploader.js
+++ src/utilities/uploader.js
@@ -3,15 +3,13 @@ const aws = require("aws-sdk");
 const multerS3 = require("multer-s3-v2");
 const path = require("path");
 const config = require("../config/config");
-const {aws_accessKeyID, aws_secretAccessKey} = config
-
+const { aws_accessKeyID, aws_secretAccessKey } = config;
 
 const s3 = new aws.S3({
   accessKeyId: aws_accessKeyID,
-  secretAccessKey: aws_secretAccessKey,  
-  region: 'us-east-1'
+  secretAccessKey: aws_secretAccessKey,
+  region: "us-east-1",
 });
-
 
 function checkFileType(file, cb) {
   // Allowed ext
@@ -30,113 +28,120 @@ function checkFileType(file, cb) {
 creating fileName
 beneficiaryId/
 */
-function uploadFileS3(fileType , bucketName) {
-  
-  if(fileType === "single") {
+function uploadFileS3(fileType, bucketName) {
+  if (fileType === "single") {
     return multer({
       storage: multerS3({
         s3: s3,
         bucket: bucketName,
-        ACL: 'public-read',
+        ACL: "public-read",
         metadata: function (req, file, cb) {
           const headers = {
-            'Content-Type': file.mimetype,
-            'Cache-Control': 'max-age=31536000',
-            'Content-Disposition': 'inline',
+            "Content-Type": file.mimetype,
+            "Cache-Control": "max-age=31536000",
+            "Content-Disposition": "inline",
           };
           cb(null, {
-             fieldName: file.fieldname,
-             ...headers 
-            });
-          
+            fieldName: file.fieldname,
+            ...headers,
+          });
         },
         key: function (req, file, cb) {
-          const {beneficiaryId} = req.params;
-          let medicalFileType = req.originalUrl.split("/")[req.originalUrl.split("/").length - 1];
-          let filePath = `${beneficiaryId}/${medicalFileType}/${Date.now().toString() + '-' + file.originalname}`;
+          const { beneficiaryId } = req.params;
+          let medicalFileType =
+            req.originalUrl.split("/")[req.originalUrl.split("/").length - 1];
+          let filePath = `${beneficiaryId}/${medicalFileType}/${
+            Date.now().toString() + "-" + file.originalname
+          }`;
           cb(null, filePath);
         },
-        contentType: multerS3.AUTO_CONTENT_TYPE
+        contentType: multerS3.AUTO_CONTENT_TYPE,
       }),
       limits: { fileSize: 5242880 }, // In bytes: 5000000 bytes = 5 MB
       fileFilter: function (req, file, cb) {
         checkFileType(file, cb);
       },
     }).single("file");
-  }else if(fileType === "multiple"){
+  } else if (fileType === "multiple") {
     return multer({
       storage: multerS3({
         s3: s3,
         bucket: bucketName,
-        ACL: 'public-read',
+        ACL: "public-read",
         metadata: function (req, file, cb) {
           const headers = {
-            'Content-Type': file.mimetype,
-            'Cache-Control': 'max-age=31536000',
-            'Content-Disposition': 'inline',
+            "Content-Type": file.mimetype,
+            "Cache-Control": "max-age=31536000",
+            "Content-Disposition": "inline",
           };
           cb(null, {
-             fieldName: file.fieldname,
-             ...headers 
-            });
-          
+            fieldName: file.fieldname,
+            ...headers,
+          });
         },
         key: function (req, file, cb) {
           // const {beneficiaryId} = req.params;
           // let medicalFileType = req.originalUrl.split("/")[req.originalUrl.split("/").length - 1];
           // let filePath = `${beneficiaryId}/${medicalFileType}/${Date.now().toString() + '-' + file.originalname}`;
-          let filePath = Date.now().toString() + '-' + file.originalname;
+          let filePath = Date.now().toString() + "-" + file.originalname;
           cb(null, filePath);
         },
-        contentType: multerS3.AUTO_CONTENT_TYPE
+        contentType: multerS3.AUTO_CONTENT_TYPE,
       }),
       limits: { fileSize: 5242880 }, // In bytes: 5000000 bytes = 5 MB
       fileFilter: function (req, file, cb) {
         checkFileType(file, cb);
       },
-    }).array("file" ,4);
+    }).array("file", 4);
   }
 }
 
-
-
-
-const getPresignedUrl = async (mainUrl , bucketName) => {
+const getPresignedUrl = async (mainUrl, bucketName) => {
   const params = {
     Bucket: bucketName,
     Key: mainUrl.split(".com/")[1],
-    ResponseContentDisposition: 'inline', // Set the disposition to inline
-    Expires: 20 // Set the URL expiration time to 10 seconds -- 
-  //   time can be changed anywhere afterwards
+    ResponseContentDisposition: "inline", // Set the disposition to inline
+    Expires: 20, // Set the URL expiration time to 10 seconds --
+    //   time can be changed anywhere afterwards
   };
   try {
-    const url = s3.getSignedUrl('getObject', params);
-    return url;  
+    const url = s3.getSignedUrl("getObject", params);
+    return url;
   } catch (error) {
     console.log(error);
-    return "";    
+    return "";
   }
-  
-}
+};
 
 const assignedPresignedUrlSingle = async (document) => {
-  if(document?.fileLink?.length) {
-    const presignedUrlArray = await Promise.all(document?.fileLink?.map(link => getPresignedUrl(link, config.dalilStorage_bucket)));
+  if (document?.fileLink?.length) {
+    const presignedUrlArray = await Promise.all(
+      document?.fileLink?.map((link) =>
+        getPresignedUrl(link, config.dalilStorage_bucket)
+      )
+    );
     document.fileLink = presignedUrlArray;
   }
   return document;
-}
+};
 
 const allDocumentsPresignedUrl = async (document) => {
-  const newDocuments = await Promise.all(document.map(async data => {
-    if(data?.fileLink.length) {
-      const presignedUrlArray = await Promise.all(data.fileLink.map(async link => await getPresignedUrl(link , config.dalilStorage_bucket)));
-      data.fileLink = presignedUrlArray;
-    }
-    return data;
-  }));
-  return newDocuments
-}
+  const newDocuments = await Promise.all(
+    document.map(async (data) => {
+      if (data?.fileLink.length) {
+        const presignedUrlArray = await Promise.all(
+          data.fileLink.map(
+            async (link) =>
+              await getPresignedUrl(link, config.dalilStorage_bucket)
+          )
+        );
+        data.fileLink = presignedUrlArray;
+      }
+      return data;
+    })
+  );
+  return newDocuments;
+};
 
 function renameKey(document, newKey, oldKey) {
   // check if the document has the old key
@@ -159,7 +164,7 @@ function renameKey(document, newKey, oldKey) {
 
   // also rename any key that matches oldKey in subdocuments recursively
   for (const [key, value] of Object.entries(newDocument)) {
-    if (typeof value === 'object' && value !== null) {
+    if (typeof value === "object" && value !== null) {
       newDocument[key] = renameKey(value, newKey, oldKey);
     }
   }
@@ -167,15 +172,20 @@ function renameKey(document, newKey, oldKey) {
   return newDocument;
 }
 
-async function returnedSingleDoc (schema, query)  {
-  const singleDocument = await schema.findOne(query).select(
-    "-__v -createdAt -updatedAt"
-  ).lean();
-  if(!singleDocument) return;
-  
-  const updatedDocument = renameKey(singleDocument , "city" , "cityId");
-  if(updatedDocument?.fileLink.length) {
-    const presignedUrlArray = await Promise.all(updatedDocument.fileLink.map(async link => await getPresignedUrl(link , config.dalilStorage_bucket)));
+async function returnedSingleDoc(schema, query) {
+  const singleDocument = await schema
+    .findOne(query)
+    .select("-__v -createdAt -updatedAt")
+    .lean();
+  if (!singleDocument) return;
+
+  const updatedDocument = renameKey(singleDocument, "city", "cityId");
+  if (updatedDocument?.fileLink.length) {
+    const presignedUrlArray = await Promise.all(
+      updatedDocument.fileLink.map(
+        async (link) => await getPresignedUrl(link, config.dalilStorage_bucket)
+      )
+    );
     updatedDocument.fileLink = presignedUrlArray;
   }
   return updatedDocument;
@@ -186,5 +196,5 @@ module.exports = {
   assignedPresignedUrlSingle,
   allDocumentsPresignedUrl,
   renameKey,
-  returnedSingleDoc
+  returnedSingleDoc,
 };
