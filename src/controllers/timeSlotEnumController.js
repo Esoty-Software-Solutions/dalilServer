64db34c017e364f87ba2e36b8b5a8be7c92c53f6
Waleed Saifi <mail.waleed.saifi@gmail.com>
2023-04-14 11:33:38 +0500

    feature:Update timeslot enum controller

--- src/controllers/timeSlotEnumController.js
+++ src/controllers/timeSlotEnumController.js
@@ -1,73 +1,87 @@
-const timeSlotEnumServices = require('../services/timeSlotEnumServices');
-const {successResponse, badRequestErrorResponse, notFoundResponse, serverErrorResponse} = require('../utilities/response');
-
-
+const {
+  successResponse,
+  badRequestErrorResponse,
+  notFoundResponse,
+  serverErrorResponse,
+} = require("../utilities/response");
+const Services = require("../services/commonServices");
+const TimeSlotEnumSchema = require("../schemas/timeSlotEnumSchema");
 
 const timeSlotEnum = {
+  // Add Time Slot Enum
 
-    // Add Time Slot Enum
+  addTimeSlotEnum: async (req, res) => {
+    try {
+      let data = await Services.createOne({
+        schemaName: TimeSlotEnumSchema,
+        body: req.body,
+      });
 
-    addTimeSlotEnum: async (req, res) => {
-        try {
-            let query = {
-                ...req.body,
-            };
-            let data = await timeSlotEnumServices.addTimeSlotEnum(query);
-
-            return successResponse(res, "Time Slot Enum Added successfully", data)
-        } catch (err) {
-            return serverErrorResponse(res, err);
-        }
-    },
-
-
-    // Get timeSlotEnum By Id
-
-    getTimeSlotEnum: async (req, res) => {
-        try {
-            let data = await timeSlotEnumServices.getTimeSlotEnum({ _id: req.params.id });
-            if(!data) return notFoundResponse(res, "No Record Found")
-            return successResponse(res, "Success", data)
-        } catch (err) {
-            return serverErrorResponse(res, err);
-        }
-    },
-
-
-    // Get list of timeSlotEnum
-
-    getAlltimeSlotEnum: async (req, res) => {
-        try {
-            let query = {
-                limit: req.query.limit,
-                skip: req.query.skip
-            }
-            let objectArray = await timeSlotEnumServices.getAllTimeSlotEnum(query);
-
-            return successResponse(res, "Success", objectArray, objectArray.length);
-        } catch (err) {
-            return serverErrorResponse(res, err);
-        }
-    },
-
-    // Update timeSlotEnum by Id
-
-    updateTimeSlotEnum: async (req, res) => {
-        try {
-            let data = await timeSlotEnumServices.updateTimeSlotEnum(
-                { _id: req.params.id },
-                {...req.body}
-            );
-
-            if (!data) {
-                return badRequestErrorResponse(res, "Record Not Found")
-              }
-
-              return successResponse(res, "Time Slot Enum updated successfully", data)
-        } catch (err) {
-            return serverErrorResponse(res, err);
-        }
+      return successResponse(res, "Time Slot Enum Added successfully", data);
+    } catch (err) {
+      return serverErrorResponse(res, err);
     }
-}
+  },
+
+  // Get timeSlotEnum By Id
+
+  getTimeSlotEnum: async (req, res) => {
+    try {
+      let query = {
+        _id: req.params.id,
+      };
+      let data = await Services.getOne({
+        schemaName: TimeSlotEnumSchema,
+        query,
+      });
+      if (!data) return notFoundResponse(res, "No Record Found");
+      return successResponse(res, "Success", data);
+    } catch (err) {
+      return serverErrorResponse(res, err);
+    }
+  },
+
+  // Get list of timeSlotEnum
+
+  getAlltimeSlotEnum: async (req, res) => {
+    try {
+      let limit = req.query.limit;
+      let skip = req.query.skip;
+      let objectArray = await Services.getMany({
+        schemaName: TimeSlotEnumSchema,
+        limit,
+        skip,
+      });
+
+      let objectCount = await Services.count({
+        schemaName: TimeSlotEnumSchema,
+      });
+      return successResponse(res, "Success", objectArray, objectCount);
+    } catch (err) {
+      return serverErrorResponse(res, err);
+    }
+  },
+
+  // Update timeSlotEnum by Id
+
+  updateTimeSlotEnum: async (req, res) => {
+    try {
+      let query = { _id: req.params.id };
+      let data = await Services.updateOne({
+        schemaName: TimeSlotEnumSchema,
+        query,
+        body: req.body,
+      });
+
+      if (!data) {
+        return badRequestErrorResponse(res, "Record Not Found");
+      }
+
+      return successResponse(res, "Time Slot Enum updated successfully", data);
+    } catch (err) {
+      return serverErrorResponse(res, err);
+    }
+  },
+};
 
 module.exports = timeSlotEnum;
