f480f6084aff8265739b52731af4b36fb6827bd3
Aarush-Virtual <115481294+Aarush-Virtual@users.noreply.github.com>
2023-04-14 10:45:00 +0530

    changed user to username in customValidation

--- src/validators/users.validator.js
+++ src/validators/users.validator.js
@@ -1,7 +1,7 @@
 const joi = require("joi");
 
 exports.loginValidator = joi.object({
-    user : joi.string().required(),
+    username : joi.string().required(),
     password : joi.string().required(),
     deviceToken : joi.string().optional(),
     deviceType : joi.string().optional().valid("android" , "ios" , "browser")
