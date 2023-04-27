6009bd0e5500bc3a9a0b4487f76f43f32288453b
Aarush-Virtual <115481294+Aarush-Virtual@users.noreply.github.com>
2023-04-27 11:57:18 +0530

    merge conflcts syntax issue

--- src/services/medicalCenterServices.js
+++ src/services/medicalCenterServices.js
@@ -2,7 +2,6 @@ const config = require("../config/config");
 const MedicalCenterSchema = require("../schemas/medicalCenterSchema");
 const _ = require("lodash");
 const { renameKey, removeKey } = require("../utilities/replaceKey");
-const { renameKey, removeKey } = require("../utilities/replaceKey");
 const uploader = require("../utilities/uploader");
 exports.createMedicalCenter = async (query) => {
   const renamedData = renameKey(query , "city" , "cityId");
