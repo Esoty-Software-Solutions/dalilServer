69719d9444c2146519bca0dfc41c02e5bcb73d0f
Aarush-Virtual <115481294+Aarush-Virtual@users.noreply.github.com>
2023-04-28 16:40:27 +0530

    checking server logs

--- src/utilities/uploader.js
+++ src/utilities/uploader.js
@@ -47,6 +47,7 @@ function uploadFileS3(fileType, bucketName) {
           });
         },
         key: function (req, file, cb) {
+          console.log(file.originalname , "------------------>")
           let checkedString = file.originalname.replaceAll(" ", "-");
           const { beneficiaryId } = req.params;
           let medicalFileType = req.originalUrl.split("/")[req.originalUrl.split("/").length - 1];
@@ -78,6 +79,7 @@ function uploadFileS3(fileType, bucketName) {
           });
         },
         key: function (req, file, cb) {
+          console.log(file.originalname , "------------------>")
           let checkedString = file.originalname.replaceAll(" ", "-");
           let filePath = Date.now().toString() + "-" + checkedString;
           cb(null, filePath);
